f606b9a229b13afd745a90c1ceee0e4a20d51c55
melikeatac <melikeatac4@gmail.com>
2024-02-04 14:08:46 +0300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.cs-main-menu .navbar-nav .nav-link').on('click', function () {
+        $('.cs-main-menu .navbar-nav .nav-link').removeClass('active');
+        $(this).addClass('active');
+    })
+    $('.cs-banner-button .cs-link-1').on('click', function () {
+        $('.cs-banner-button .cs-link-1').removeClass('active');
+        $(this).addClass('active');
+    })
+    $('.cs-sec5-list .cs-item a').on('click', function () {
+        $('.cs-sec5-list .cs-item a').removeClass('active');
+        $(this).addClass('active');
+    })
+})
